abd01f95f03789c7d18780b63c2625e8f384733f
romax114 <70773814+romax114@users.noreply.github.com>
2020-10-25 23:18:11 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    while(str){
+        let counter = 0
+        for(const i of bracketsConfig) {
+            let brackets = i[0] + i[1]
+            if(str.includes(brackets)){
+                str = str.replace(brackets, '');
+                counter += 1;
+            }
+        }
+        if(counter == 0)
+            break;
+    }
+    return str == '' ? true : false
 }
